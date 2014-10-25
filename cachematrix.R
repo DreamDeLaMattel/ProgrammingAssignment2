706ca9d5e4b45783d9c23e71e039e7c595271134
Dreamdelamattel <los400golpes@hotmail.es>
2014-10-25 14:12:11 +0200

    done exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## the purpose of this functions is to return de inverse of the input matrix
+## if the solution was already operated it will save time returning the result from cache
 
-## Write a short comment describing this function
+## this function operates the inverse of the input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  inverse<-NULL
+  set<-function(y){  
+  x<<-y
+  inverse<<-NULL}
 
-}
+get<-function()x
+setinverse<-function(solve) inverse<<-solve
+getinverse<-function()inverse 
+list(set = set, get = get,
+     setinverse = setinverse,
+     getinverse = getinverse)}
 
 
-## Write a short comment describing this function
+## this function if "inverse" is not null will return us the inverse, if it's null will get the inverse from the previous function
 
 cacheSolve <- function(x, ...) {
+  inverse <-x$getinverse()
+  if(!is.null(inverse)){
+    message("getting cached data")
+    return(inverse)
+    
+  }
+  data<-x$get()
+  inverse<-solve(data, ...)
+  x$setinverse(inverse)
+  inverse
+  
         ## Return a matrix that is the inverse of 'x'
 }
